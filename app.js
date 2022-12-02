f3512e3e1621f5bbc1877144aff7a7de630c25db
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-12-02 21:37:29 +0100

    implement functianolity to scroll to direct section after click button

--- app.js
+++ app.js
@@ -30,3 +30,26 @@ const headerEl = document.querySelector(".header");
 btnNav.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
+
+// smooth scroling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
